4c8bc10d14d61599e9eeca8c227e265adf93555d
sinclairstudy <sinclairstudy@gmail.com>
2018-03-18 14:42:51 -0400

    Done.

--- db/migrate/20180318174311_create_students.rb
+++ db/migrate/20180318174311_create_students.rb
@@ -3,8 +3,7 @@ class CreateStudents < ActiveRecord::Migration
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
-
-      t.timestamps null: false
+      t.integer :school_class_id
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+s = SchoolClass.create([{title: "Class 1", room_number: 2}, {title: "Class 2", room_number: 13}])
+Student.create([{first_name: "Sinclair", last_name: "Kinch", school_class: s[0]},
+  {first_name: "Hannah", last_name: "Kinch", school_class: s[0]},
+  {first_name: "Georgia", last_name: "Kinch", school_class: s[0]}])
+Student.create([{first_name: "Gregory", last_name: "Kinch", school_class: s[1]},
+  {first_name: "Peg", last_name: "Kinch", school_class: s[1]},
+  {first_name: "Travis", last_name: "Kinch", school_class: s[1]}])
